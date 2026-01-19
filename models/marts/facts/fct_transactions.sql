314a44bbce25679d8c8a2aa95e79af5689c447d6
Arun Chandar <arunchandar100@gmail.com>
2026-01-19 02:19:41 +0000

    Converted fct_transactions to incremental (merge)

--- models/marts/facts/fct_transactions.sql
+++ models/marts/facts/fct_transactions.sql
@@ -1,5 +1,12 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='transaction_line_id'
+    )
+}}
+
 with transactions as (
-select * from {{ ref('stg_transactions') }}
+    select * from {{ ref('stg_transactions') }}
 ), 
 
 customers as (
@@ -53,11 +60,18 @@ select
     t.target_currency_amount, 
     t.market_rate, 
     t.exchange_rate_applied, 
-    t.fee_amount
+    t.fee_amount,
+
+    --metadata
+    t.loaded_at
 
 from transactions t 
 left join customers c on t.customer_id = c.customer_id
 left join employees e on t.employee_id = e.employee_id
 left join branches b on t.branch_id = b.branch_id
-left join currencies src on  t.source_currency_code = src.currency_code 
-left join currencies tgt on t.target_currency_code = tgt.currency_code 
+left join currencies src on t.source_currency_code = src.currency_code 
+left join currencies tgt on t.target_currency_code = tgt.currency_code
+
+{% if is_incremental() %}
+    where t.loaded_at > (select max(loaded_at) from {{ this }})
+{% endif %}
